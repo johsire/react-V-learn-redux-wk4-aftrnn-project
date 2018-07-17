b3086bfd90adeb1aa8b42b7b5b6972680c4db29b
Josiah Maina <josiah5115@gmail.com>
2018-07-16 20:59:48 -0600

    update the onClick events to call updateProp instead

--- src/components/WizardThree/WizardThree.js
+++ src/components/WizardThree/WizardThree.js
@@ -6,22 +6,32 @@ import { updateProp } from '../../ducks/reducer.js';
 
 class WizardThree extends Component {
     render(){
-        return(
-            <div className="parent-div">
-                <div className="vert-align">
-                   <p> What property are you looking to use the loan on? </p><br />
-                    <div className="row">
-                        <Link to="/wFour"><button value="primaryHome" onClick={this.props.propToBeUsedOn}>Primary Home</button></Link>
-                        <Link to="/wFour"><button value="rentalProperty" onClick={this.props.propToBeUsedOn}>Rental Property</button></Link>
-                        <Link to="/wFour"><button value="secondaryHome" onClick={this.props.propToBeUsedOn}>Secondary Home</button></Link>
-                    </div>
-                </div>           
-            </div>
-        )
-    }
-}
+      const { updateProp } = this.props;
 
-function mapStateToProps( state ) {
+      return (
+        <div className="parent-div">
+          <div className="vert-align">
+            <p> What property are you looking to use the loan on? </p><br />
+            <div className="row">
+              <Link to="/wFour">
+                <button value="primaryHome" onClick={ ( e ) => updateProp( e.target.value ) }>Primary Home</button>
+              </Link>
+  
+              <Link to="/wFour">
+                <button value="rentalProperty" onClick={ ( e ) => updateProp( e.target.value ) }>Rental Property</button>
+              </Link>
+  
+              <Link to="/wFour">
+                <button value="secondaryHome" onClick={ ( e ) => updateProp( e.target.value ) }>Secondary Home</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      )
+    }
+  }
+
+ function mapStateToProps( state ) {
     const { propToBeUsedOn } = state;
   
     return {
