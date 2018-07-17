145addb12044f1d022b12bda37e0664a674f08d4
Josiah Maina <josiah5115@gmail.com>
2018-07-16 19:59:52 -0600

    fixed some bugs

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-
-import store from './store';
 import App from './App';
 import './index.css';
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
     <HashRouter>
       <App />
     </HashRouter>
-  </Provider>
-,document.getElementById('root'));
+  </Provider>    
+, document.getElementById('root'));
