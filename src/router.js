9521989f083a173085422180bd18f3121da2e327
Josiah Maina <josiah5115@gmail.com>
2018-07-16 19:05:43 -0600

    set the settings in reducer file

--- src/router.js
+++ src/router.js
@@ -14,59 +14,6 @@ import Finish from './components/Finish/Finish';
 
 import NextBtn from './components/NextBtn/NextBtn';
 import { HashRouter, Route } from 'react-router-dom';
-import reducer from './ducks/reducer';
-
-
-const initialState = {
-    loanType: 'Home Purchase',
-    propertType: 'Single Family Home',
-    city: '',
-    propToBeUsedOn: '',
-    found: "false",
-    realEstateAgent: "false",
-    cost: 0,
-    downPayment: 0,
-    credit: '',
-    history: '',
-    addressOne: '',
-    addressTwo: '',
-    addressThree: '',
-    firstName: '',
-    lastName: '',
-    email: ''
-}
-
-const UPDATE_LOAN_TYPE = "UPDATE_LOAN_TYPE";
-const UPDATE_PROPERTY_TYPE = 'UPDATE_PROPERTY_TYPE';
-
-function reducer(state = initialState, action) {
-   switch(action.type) {
-       case UPDATE_LOAN_TYPE:
-            return Object,assigbn=({}, state, { loanType: action.payload });
-
-        case  UPDATE_PROPERTY_TYPE:
-            return Object,assigbn=({}, state, { loanType: action.payload });
-
-        default: return state;     
-   }
-}
-
-export function updateLoanType(loanType) {
-    return {
-        type: UPDATE_LOAN_TYPE,
-        payload: loanType
-    }
-}
-
-export function updatePropertyType(property) {
-    return {
-        type: UPDATE_PROPERTY_TYPE,
-        payload: loanType
-    }
-}
-
-export default reducer;
-
 
 export default (
     <HashRouter>
